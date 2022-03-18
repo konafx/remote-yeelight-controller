aa64766ae5f98fce9edf5e5a93722124cd92f116
konafx <22848261+konafx@users.noreply.github.com>
2022-03-18 14:10:55 +0900

    Add switch
    - add bulma-switch
    - handle reference: https://zenn.dev/bom_shibuya/articles/bd9c84bfe59f4f
    - unique id reference: https://stackoverflow.com/questions/29420835/how-to-generate-unique-ids-for-form-labels-in-react

---
+++ src/Components/Toggler.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { getHtmlId } from 'Utils';
+import 'bulma-switch/dist/css/bulma-switch.min.css';
+
+type Props = {
+  label: string;
+  check: boolean;
+  onChange: (check: boolean) => void;
+}
+
+export const Toggler = (props: Props) => {
+  const [id] = useState(() => getHtmlId());
+
+  const handle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChange(!props.check);
+  };
+
+  return (
+    <div className="field">
+      <input id={id} type="checkbox" className="switch control"
+        onChange={handle} checked={props.check}
+      />
+      <label htmlFor={id}>{props.label}</label>
+    </div>
+  );
+};
