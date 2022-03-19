3e15de2fc11dd323eb1de34db3062a3b42b272c8
konafx <22848261+konafx@users.noreply.github.com>
2022-03-19 15:21:08 +0900

    Add eslint, prettier

--- src/components/Activeness.tsx
+++ src/components/Activeness.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
-import { useAppSelector, useAppDispatch } from "hooks";
-import {Toggler} from "./Toggler";
-import { activate, deactivate } from "slices/assassin";
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from 'hooks';
+import { Toggler } from './Toggler';
+import { activate, deactivate } from 'slices/assassin';
 
 export const Activeness = () => {
   const activeness = useAppSelector((state) => state.assassin.value);
@@ -11,20 +11,17 @@ export const Activeness = () => {
   const timeMax = 10;
   const timeMin = 2;
   useEffect(() => {
-    // TODO: dev 
-    const time = Math.floor(Math.random() * (timeMax-timeMin) + timeMin);
-    console.debug(`activeness [${time}] toggler start!`)
+    // TODO: dev
+    const time = Math.floor(Math.random() * (timeMax - timeMin) + timeMin);
+    console.debug(`activeness [${time}] toggler start!`);
     setTimeout(() => {
-      dispatch(activeness ? deactivate() : activate())
+      dispatch(activeness ? deactivate() : activate());
       console.debug(`STOP activeness [${time}] toggler`);
-    }, time*1000);
+    }, time * 1000);
   }, [activeness, dispatch]);
+  function emp() {
+    // do nothing
+  }
 
-  return <Toggler
-    check={activeness}
-    onChange={()=>function(){}}
-    label={activeness ? 'Active' : 'Deactive'}
-    disabled
-    />;
+  return <Toggler check={activeness} onChange={() => emp()} label={activeness ? 'Active' : 'Deactive'} disabled />;
 };
-
