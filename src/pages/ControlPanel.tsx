5623d4bc6e9342330b86cb9b769d1483814c16b7
konafx <afxlieonon@gmail.com>
2022-03-19 15:23:42 +0900

    Change centering
    - fix debouncedpicker to allow props.className
      - ref https://github.com/omgovich/react-colorful/blob/a85e5b36b55cae7e95c73c8ecde0bc881e8e3b1f/src/types.ts#L54-L57

--- src/pages/ControlPanel.tsx
+++ src/pages/ControlPanel.tsx
@@ -1,21 +1,31 @@
-import {useState} from 'react';
-import {Toggler} from 'components/Toggler';
-import {DebouncedPicker} from 'components/DebouncedPicker';
-import {Activeness} from 'components/Activeness';
+import { useState, useEffect, useRef } from 'react';
+import { Toggler } from 'components/Toggler';
+import { DebouncedPicker } from 'components/DebouncedPicker';
+import { Activeness } from 'components/Activeness';
+import { Slider } from 'components/Slider';
 
 const ControlPanel = () => {
+  const inputRef = useRef(null);
   const [check, setCheck] = useState(false);
   const [color, setColor] = useState('#f0f0f0');
+  const [brightness, setBrightness] = useState(50);
 
   return (
     <section className="section">
       <div className="container">
-        <div className="content">
+        <div className="content has-text-centered">
           <Activeness />
-          <Toggler check={check} onChange={() => setCheck(!check)} label="Light"/>
-          <p>current switch is: {check ? "on":"off"}</p>
-          <DebouncedPicker color={color} onChange={setColor} time={1000} />
+          <Toggler check={check} onChange={() => setCheck(!check)} label="Light" />
+          <p>current switch is: {check ? 'on' : 'off'}</p>
+          <DebouncedPicker color={color} onChange={setColor} time={1000} className="mx-auto" />
           <p>current color is: {color}</p>
+          <div className="columns is-mobile is-vcentered">
+            <div className="column is-one-fifth has-text-right"> 0 </div>
+            <div className="column">
+              <Slider ref={inputRef} value={brightness} onChange={setBrightness} min={0} max={100} />
+            </div>
+            <div className="column is-one-fifth has-text-left">100</div>
+          </div>
         </div>
       </div>
     </section>
