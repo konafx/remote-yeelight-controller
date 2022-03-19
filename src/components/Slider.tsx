a42ac0dd0e2b1abbd7d00cad78f238b7ed50eb5d
konafx <afxlieonon@gmail.com>
2022-03-19 15:22:45 +0900

    Add slider

---
+++ src/components/Slider.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import 'bulma-slider/dist/css/bulma-slider.min.css';
+
+type Props = {
+  value: number;
+  onChange: (value: number) => void;
+  max: number;
+  min: number;
+  step?: number;
+  ref?: React.MutableRefObject<null>;
+};
+
+export const Slider = (props: Props) => {
+  const handler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue: number = +e.target.value;
+    props.onChange(newValue);
+  };
+
+  return (
+    <input
+      className="slider is-fullwidth"
+      type="range"
+      max={props.max}
+      min={props.min}
+      step={props.step ?? 1}
+      value={props.value}
+      onChange={handler}
+      ref={props.ref}
+    />
+  );
+};
